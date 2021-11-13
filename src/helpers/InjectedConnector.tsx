2929ae6de30fbd223b1f4ff506868205212fa0d2
magnuslundstrom <magnuslundstromdk@gmail.com>
2021-11-13 14:56:57 +0100

    setup env-provider

--- src/helpers/InjectedConnector.tsx
+++ src/helpers/InjectedConnector.tsx
@@ -1,5 +1,6 @@
 import { InjectedConnector as Web3InjectedConnector } from '@web3-react/injected-connector';
+import { currentChains } from '@/constants/chains';
 
 export const InjectedConnector = new Web3InjectedConnector({
-  supportedChainIds: [3],
+  supportedChainIds: currentChains,
 });
