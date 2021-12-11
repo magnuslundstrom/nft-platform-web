6c8a54ee76096c1dcc8f7e9c1aafcb3a9668af8a
Magnus Lundstrom <magnuslundstromdk@gmail.com>
2021-12-11 19:46:37 +0100

    init

---
+++ tests/e2e/specs/test.spec.js
@@ -0,0 +1,6 @@
+describe('wallet tests', () => {
+  // eslint-disable-next-line ui-testing/missing-assertion-in-test
+  it('should login i guess', () => {
+    cy.visit('http://localhost:3000');
+  });
+});
