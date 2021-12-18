18879f0971ccf5a67383de429301120f462190b8
Magnus Lundstrom <magnuslundstromdk@gmail.com>
2021-12-18 21:12:09 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,10 @@ import Web3Provider from '@/contexts/Web3';
 import ThemeProvider from '@/contexts/Theme';
 import ContractProvider from '@/contexts/Contracts';
 import { FeedbackProvider } from '@/contexts/Feedback';
+import { TARGET_ENV } from '@/constants/env';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log(TARGET_ENV);
   return (
     <ThemeProvider>
       <CssBaseline />
